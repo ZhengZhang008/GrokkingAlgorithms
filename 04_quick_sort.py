97e41666b5d77c2da0d6765bf7a95003785f027a
Zheng Zhang <95887742+ZhengZhang008@users.noreply.github.com>
2023-08-23 20:59:47 +1000

    Update 04_quick_sort.py

--- 04_quick_sort.py
+++ 04_quick_sort.py
@@ -25,10 +25,14 @@ def find_max(list):
 
 def quick_sort(array):
     if len(array) < 2:
+        # base case, arrays with 0 or 1 element are already "sorted"
         return array
     else:
+        # recursive case
         pivot = array[0]
+        # sub-array of all the elements less than the pivot
         less = [i for i in array[1:] if i <= pivot]
+        # sub-array of all the elements greater than the pivot
         greater = [i for i in array[1:] if i > pivot]
         return quick_sort(less) + [pivot] + quick_sort(greater)
 
