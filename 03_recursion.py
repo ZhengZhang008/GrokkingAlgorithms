3444e12b3dc7a201266e6b6bcb32a880a25d9d05
Zheng <zz1996hku@gmail.com>
2023-08-30 20:25:56 +1000

    Update 03_recursion.py

--- 03_recursion.py
+++ 03_recursion.py
@@ -1,9 +1,9 @@
 def countdown(i):
     print(i)
-    # base case
+    # base case: if i is less than or equal to 0, stop the recursion
     if i <= 0:
         return
-    # recursive case
+    # recursive case: if i is greater than 0, call the function again with i-1
     else:
         countdown(i - 1)
 
@@ -24,8 +24,10 @@ def bye():
 
 
 def fact(x):
+    # base case: if x is 1, return 1
     if x == 1:
         return 1
+    # recursive case: if x is greater than 1, return x multiplied by the factorial of x-1
     else:
         return x * fact(x - 1)
 
