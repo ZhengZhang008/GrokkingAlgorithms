71d281454800edf4c0ad1f9b9ef367c7500268fb
Zheng <zz1996hku@gmail.com>
2023-08-30 20:23:02 +1000

    Update 02_selection_sort.py

--- 02_selection_sort.py
+++ 02_selection_sort.py
@@ -5,19 +5,24 @@ def find_smallest(arr):
     # Stores the index of the smallest value
     smallest_index = 0
     for i in range(1, len(arr)):
+        # If the current element is smaller than the smallest found so far
         if arr[i] < smallest:
+            # Update the smallest value and its index
             smallest = arr[i]
             smallest_index = i
+    # Return the index of the smallest element
     return smallest_index
 
 
-# Sort array
 def selection_sort(arr):
+    # Initialize a new array to store the sorted elements
     new_arr = []
     for i in range(len(arr)):
-        # Finds the smallest element in the array and adds it to the new array
+        # Finds the smallest element in the array
         smallest = find_smallest(arr)
+        # Remove the smallest element from the original array and add it to the new array
         new_arr.append(arr.pop(smallest))
+    # Return the sorted array
     return new_arr
 
 
