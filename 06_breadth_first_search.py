086840da21583fc57228144e49e624704ab2e803
Zheng <zz1996hku@gmail.com>
2023-08-31 11:01:35 +1000

    Update 06_breadth_first_search.py

--- 06_breadth_first_search.py
+++ 06_breadth_first_search.py
@@ -2,22 +2,28 @@ from collections import deque
 
 
 def search(name):
+    # Initialize a queue with the starting name
     search_queue = deque()
     search_queue += [name]
     # This is how you keep track of which people you've searched before.
     searched = []
+    # Continue searching while the queue is not empty
     while search_queue:
+        # Pop a person from the left of the queue
         person = search_queue.popleft()
         # Only search this person if you haven't already searched them.
         if person in searched:
             continue
+        # If the person is a seller, print their name and return True
         if person_is_seller(person):
             print(person + " is a mango seller!")
             return True
         else:
+            # If the person is not a seller, add their neighbors to the queue
             search_queue += graph[person]
             # Marks this person as searched
             searched.append(person)
+    # If no seller is found, return False
     return False
 
 
